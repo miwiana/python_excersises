b8ad69c8434864525b3b2dc1b1a886323572c140
agrymanpurta <agnieszka.gryman-purta@relayr.io>
2022-01-27 20:47:07 +0100

    add methods to sort & limit output

--- ex_2/calculations.py
+++ ex_2/calculations.py
@@ -29,9 +29,34 @@ def calculate_total_price_in_pln(currencies, data_rows):
     return data_rows
 
 
+def get_rows_with_proper_matching_id(data_rows, matching_id):
+    output = []
+    for row in data_rows:
+        if int(row[4]) == matching_id:
+            output.append(row)
+    return output
+
+
+def sort_rows_by_price_desc(data_rows):
+    sorted_data = sorted(data_rows, key=lambda x: float(x[1]), reverse=True)
+    return sorted_data
+
+
+def get_top_priced_rows(data_rows: list, rows_amount: int):
+    return data_rows[:rows_amount]
+
+
 if __name__ == "__main__":
+    # read files
     data_header, data_rows = read_csv_file("data.csv")
     curr_header, curr_rows = read_csv_file("currencies.csv")
     match_header, match_rows = read_csv_file("matchings.csv")
+    # unify data
     unified_data_rows = calculate_total_price_in_pln(curr_rows, data_rows)
+
+    # get data
+    matched_rows = get_rows_with_proper_matching_id(unified_data_rows, 1)
+    sorted_rows = sort_rows_by_price_desc(matched_rows)
+    top_rows = get_top_priced_rows(sorted_rows, 2)
+    print(top_rows)
 
