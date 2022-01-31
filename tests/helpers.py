bd15ae4cc3ec4c87fca0a20f5d1b9a1a0321da7e
agrymanpurta <agnieszka.gryman-purta@relayr.io>
2022-01-31 20:11:42 +0100

    add one more test

--- tests/helpers.py
+++ tests/helpers.py
@@ -20,3 +20,32 @@ def get_rows(path_to_file):
     headers, rows = read_csv_file(path_to_file)
     return rows
 
+
+def get_rows_with_matching_id_1(output_file_path):
+    rows_with_matching_id_1 = []
+
+    rows = get_rows(output_file_path)
+    for row in rows:
+        if int(row[4]) == 1:
+            rows_with_matching_id_1.append(row)
+    return rows_with_matching_id_1
+
+
+def get_rows_with_matching_id_2(output_file_path):
+    rows_with_matching_id_2 = []
+
+    rows = get_rows(output_file_path)
+    for row in rows:
+        if int(row[4]) == 2:
+            rows_with_matching_id_2.append(row)
+    return rows_with_matching_id_2
+
+
+def get_rows_with_matching_id_3(output_file_path):
+    rows_with_matching_id_3 = []
+
+    rows = get_rows(output_file_path)
+    for row in rows:
+        if int(row[4]) == 3:
+            rows_with_matching_id_3.append(row)
+    return rows_with_matching_id_3
--- tests/test_prices.py
+++ tests/test_prices.py
@@ -1,12 +1,18 @@
 import pytest
-from os import path
+from os.path import exists
 
-from python_excersises.tests.helpers import get_headers, get_rows
+from python_excersises.tests.helpers import (
+    get_headers,
+    get_rows,
+    get_rows_with_matching_id_1,
+    get_rows_with_matching_id_2,
+    get_rows_with_matching_id_3,
+)
 
 
 class TestExcerciseTwo:
     def test_check_result_file(self, output_file_path):
-        file = path.exists(output_file_path)
+        file = exists(output_file_path)
         assert file, "Output file does not exist in expected path."
 
     def test_check_headers_of_result_file(self, output_file_path):
@@ -24,4 +30,17 @@ class TestExcerciseTwo:
 
     def test_check_entries_amount(self, output_file_path):
         rows_amount = len(get_rows(output_file_path))
-        assert rows_amount == 7, "Rows amount is not equal to expected total top_priced_count"
+        assert (
+            rows_amount == 7
+        ), "Rows amount is not equal to expected total top_priced_count"
+
+    def test_check_entries_amount_per_each_matching_id(self, output_file_path):
+        assert (
+            len(get_rows_with_matching_id_1(output_file_path)) == 2
+        ), "Entries with matching_id=1 should be 2"
+        assert (
+            len(get_rows_with_matching_id_2(output_file_path)) == 2
+        ), "Entries with matching_id=2 should be 2"
+        assert (
+            len(get_rows_with_matching_id_3(output_file_path)) == 3
+        ), "Entries with matching_id=3 should be 3"
